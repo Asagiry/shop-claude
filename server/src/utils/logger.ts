8d66adb4eaccb585e7879c5aa67da1c3c72df80e
voimaxis@mail.ru <voimaxis@mail.ru>
2026-05-28 20:30:39 +0300

    fix: move server.log to project root directory

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const LOG_FILE = path.join(__dirname, '../../server.log');
+const LOG_FILE = path.join(__dirname, '../../../server.log');
 
 export const logEvent = (event: string, details?: string) => {
   const timestamp = new Date().toISOString();
